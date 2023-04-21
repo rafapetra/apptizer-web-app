38a4178756eb6e2d582d035f2de7a233bbe43086
Rafael Petrachini <raffp@me.com>
2023-04-21 10:29:00 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Apptizer</h2>
+        </header>
+        
+    )
+}
