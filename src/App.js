e32782c65a30de6af426439b00bd16b0f9ffe4f2
Rafael Petrachini <raffp@me.com>
2023-04-21 10:35:26 -0700

    add initial three components: Header, Menu and Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Header from "./components/Header"
+import Menu from "./components/Menu"
+import Content from "./components/Content"
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Menu />
+            <Content />
         </div>
     )
 }
---
+++ src/components/Content.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Content() {
+    return (
+            <h2>Content</h2>
+        
+    )
+}
